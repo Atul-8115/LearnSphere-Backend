1f0f9109ba63341f43e51b27b12ba6fe5a508c40
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-24 12:58:58 +0530

    chore: Update package.json to use src/index.js as the main file and start the server with 'node'

--- src/config/razorpay.js
+++ src/config/razorpay.js
@@ -1,4 +1,10 @@
 import Razorpay from "razorpay"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+console.log('RAZORPAY_KEY_ID:', process.env.RAZORPAY_KEY);
+console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_SECRET);
 
 const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
